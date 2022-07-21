b802873d135ceec2471d73f5ecf49bb779d46295
mrgold87 <kirgiz_69@bk.ru>
2022-07-21 16:44:40 +0300

    fix_21_07_22

--- readme.md
+++ readme.md
@@ -4,6 +4,8 @@
 3. Активируем тему в админ-панели WordPress -сайта: http://your_site.loc/wp-admin/themes.php
 4. Устанавливаем плагин ContactForm 7 в админ-панели WordPress -сайта: http://your_site.loc/wp-admin/plugins.php
 5. Импортируем форму обратной связи на странице импорта: http://your_site.loc/wp-admin/import.php Выбираем пунк  WordPress> запустить импорт. Загружаем файл k-telecom.WordPress.2022-07-17.xml из корня проекта.
-6. В вижжетах:http://your_site.loc/wp-admin/widgets.php находим виджет Contact Form 7, перемещаем его в 'область под контактную форму' выбираем форму 'контактная форма1'. Сохраняем изменения.
-7. Заполняем слайдер несколькими слайдами на странице: http://your_site.loc/wp-admin/edit.php?post_type=slider
-8. Заполняем раздел 'тарифы' несколькими тарифами на странице:http://your_site.loc/wp-admin/edit.php?post_type=tariff
+6. Устанавливаем плагин advance custom fields 
+7. На странице http://your_site.loc/wp-admin/edit.php?post_type=acf-field-group&page=acf-tools импортируем файл acf-export-2022-07-21.json из корня проекта
+8. Создаём страницу(http://your_site.loc/wp-admin/edit.php?post_type=page). Выбираем для неё шаблон 'Front Page'. Внизу страницы редактирования появляется select-поле с заголовком Контактная форма, где необходимо выбрать значение 'Контактная форма 1'. Сохраняем страницу.
+9. Заполняем слайдер несколькими слайдами на странице: http://your_site.loc/wp-admin/edit.php?post_type=slider
+10. Заполняем раздел 'тарифы' несколькими тарифами на странице:http://your_site.loc/wp-admin/edit.php?post_type=tariff
--- wp-content/themes/telecom/front-page.php
+++ wp-content/themes/telecom/front-page.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Template Name: Front Page
+ */
+
 get_header();
 $slider_query = new WP_Query(array(
     "post_type" => "slider",
@@ -7,36 +11,36 @@ $slider_query = new WP_Query(array(
 ?>
     <section class="slider">
         <?php if ($slider_query->have_posts()) : ?>
-        <div class="slider__container owl-carousel slider-carousel">
-            <?php
-            while ($slider_query->have_posts()) : $slider_query->the_post();
-                ?>
-                <div class="slider__row">
-                    <div class="slider__description">
-                        <h2 class="slider__title"><?php the_title(); ?> </h2>
-                        <div class="slider__caption">
-                            <?php the_content('', false); ?>
+            <div class="slider__container owl-carousel slider-carousel">
+                <?php
+                while ($slider_query->have_posts()) : $slider_query->the_post();
+                    ?>
+                    <div class="slider__row">
+                        <div class="slider__description">
+                            <h2 class="slider__title"><?php the_title(); ?> </h2>
+                            <div class="slider__caption">
+                                <?php the_content('', false); ?>
+                            </div>
+                            <a href="<?php the_permalink(); ?>" class="button slider__button">Подробнее</a>
                         </div>
-                        <a href="<?php the_permalink(); ?>" class="button slider__button">Подробнее</a>
+                        <div class="slider__image">
+                            <?php
+                            if (has_post_thumbnail()):
+                                $post_id = get_the_ID();
+                                $thumbnail_id = get_post_meta($post_id, '_thumbnail_id', true);
+                                ?>
+                                <img src="<?php echo wp_get_attachment_image_url($thumbnail_id, 'slider-thumb'); ?>"
+                                     alt="<?php the_title(); ?>"/>
+                            <?php endif; ?>
+                        </div>
                     </div>
-                    <div class="slider__image">
-                        <?php
-                        if (has_post_thumbnail()):
-                            $post_id = get_the_ID();
-                            $thumbnail_id = get_post_meta($post_id, '_thumbnail_id', true);
-                            ?>
-                            <img src="<?php echo wp_get_attachment_image_url($thumbnail_id, 'slider-thumb'); ?>"
-                                 alt="<?php the_title(); ?>"/>
-                        <?php endif; ?>
-                    </div>
-                </div>
-            <?php
-            endwhile;
+                <?php
+                endwhile;
                 wp_reset_postdata();
                 ?>
-        </div>
+            </div>
         <?php else: ?>
-        <div class="page__container"> <p><?php echo 'Слайдер пуст...' ?></p></div>
+            <div class="page__container"><p><?php echo 'Слайдер пуст...' ?></p></div>
         <?php endif; ?>
     </section>
 <?php
@@ -47,48 +51,58 @@ $tariff_query = new WP_Query(array(
 ?>
     <section class="tariff">
         <div class="tariff__container">
-            <?php if ($tariff_query->have_posts()) :  ?>
-            <h2 class="tariff__title">Тарифные планы</h2>
-            <div class="owl-carousel  tariff-carousel">
-                <?php
-               while ($tariff_query->have_posts()) : $tariff_query->the_post();
-                    ?>
-                    <div class="tariff__item item">
-                        <h3 class="item__title"><?php the_title(); ?></h3>
-                        <span class="item__caption">Скорость интернета</span>
-                        <h4 class="item__speed"><?php echo telecom_get_tariff_speed(get_the_ID()); ?> Мбит/с</h4>
-                        <div class="item__description">
-                            <?php the_content('', false); ?>
-                        </div>
-                        <div class="item__tv">
-                            <div class="item__icon-<?php echo telecom_get_toggle_tv(get_the_ID()); ?>"></div>
-                            <div class="item__basic">
-                                <span>ТВ + 100 ₽</span>
-                                <span>(пакет “Базовый”)</span>
+            <?php if ($tariff_query->have_posts()) : ?>
+                <h2 class="tariff__title">Тарифные планы</h2>
+                <div class="owl-carousel  tariff-carousel">
+                    <?php
+                    while ($tariff_query->have_posts()) : $tariff_query->the_post();
+                        $tariff_speed = get_field('tariff_speed');
+                        $tariff_tv = get_field('tariff_tv');
+                        $tariff_price = get_field('tariff_price');
+                        $tariff_footnote = get_field('tariff_footnote');
+                        $tariff_tv = $tariff_tv[0];
+                        ?>
+                        <div class="tariff__item item">
+                            <h3 class="item__title"><?php the_title(); ?></h3>
+                            <span class="item__caption">Скорость интернета</span>
+                            <h4 class="item__speed"><?php echo $tariff_speed ? $tariff_speed : ''; ?> Мбит/с</h4>
+                            <div class="item__description">
+                                <?php the_content('', false); ?>
                             </div>
-                        </div>
+                            <div class="item__tv">
+                                <div class="item__icon-<?php if ($tariff_tv =='Да') {echo 'on';} else{echo 'off';} ?>"></div>
+                                <div class="item__basic">
+                                    <span>ТВ + 100 ₽</span>
+                                    <span>(пакет “Базовый”)</span>
+                                </div>
+                            </div>
 
-                        <div class="item__price"><span><?php echo telecom_get_tariff_price(get_the_ID()); ?> ₽</span>
-                            <div>в месяц</div>
-                        </div>
-                        <div class="item__footnote">
-                            *<?php echo telecom_get_tariff_footnote(get_the_ID()); ?>
-                        </div>
-                        <a href="<?php the_permalink(); ?>" class="button button_all-width">Выбрать тариф</a>
+                            <div class="item__price">
+                                <span><?php echo $tariff_price ? $tariff_price : ''; ?> ₽</span>
+                                <div>в месяц</div>
+                            </div>
+                            <div class="item__footnote">
+                                <?php echo $tariff_footnote ? '* '.$tariff_footnote : ''; ?>
+                            </div>
+                            <a href="<?php the_permalink(); ?>" class="button button_all-width">Выбрать тариф</a>
 
-                    </div>
-                <?php
-                endwhile;
+                        </div>
+                    <?php
+                    endwhile;
+                    wp_reset_postdata();
                     ?>
 
-            </div>
+                </div>
             <?php else: ?>
 
-                <div class="page__container">  <p><?php echo 'Тарифов нет...' ?></p></div>
+                <div class="page__container"><p><?php echo 'Тарифов нет...' ?></p></div>
             <?php endif; ?>
         </div>
     </section>
-<?php if (is_active_sidebar('contact-form-widget-area')) : ?>
+<?php
+$form_id = get_field('contact_form');
+if (!empty($form_id)):
+    ?>
     <section class="request">
         <div class="request__container">
             <div class="form">
@@ -100,7 +114,7 @@ $tariff_query = new WP_Query(array(
                     <a class="" href="#" data-service="Видеонаблюдение">Видеонаблюдение</a>
                 </div>
                 <div class="contact-form-7">
-                    <?php dynamic_sidebar('contact-form-widget-area'); ?>
+                    <?php echo do_shortcode("[contact-form-7 id=$form_id] "); ?>
                 </div>
             </div>
         </div>
